3dac7048399e4722c937e20fd7ce35ae1e505d89
Terrence O'Connor <terrenceoconnor@Terrences-MBP.fios-router.home>
2020-02-11 17:59:03 -0500

    Added hidden next button and changing background color

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
+var firstbutton = document.getElementById("first");
+var secondbutton = document.getElementById("second");
+var thirdbutton = document.getElementById("third");
+var fourthbutton = document.getElementById("fourth");
 
 
 
 
-document.getElementById("first").addEventListener("click", correctAnswer);
-document.getElementById("second").addEventListener("click", incorrectAnswer);
-document.getElementById("third").addEventListener("click", incorrectAnswer);
-document.getElementById("fourth").addEventListener("click", incorrectAnswer);
+firstbutton.addEventListener("click", correctAnswer);
+secondbutton.addEventListener("click", incorrectAnswer);
+thirdbutton.addEventListener("click", incorrectAnswer);
+fourthbutton.addEventListener("click", incorrectAnswer);
 
-function correctAnswer(){
-  document.getElementById("correct").innerHTML = "CORRECT!";
+
+
+
+function correctAnswer() {
+    document.getElementById("correct").innerHTML = "CORRECT!";
 }
 
-function incorrectAnswer(){
-  document.getElementById("incorrect").innerHTML = "Incorrect";
+function incorrectAnswer() {
+    document.getElementById("incorrect").innerHTML = "Incorrect";
+}
+
+function showNext() {
+    document.getElementById('next').style.display = 'block';
+}
+
+
+
+function greenBack() {
+    document.getElementById("questiongrid").style.backgroundColor = "green";
+}
+
+function redBack() {
+
 }
